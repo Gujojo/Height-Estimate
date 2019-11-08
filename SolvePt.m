d06efa8ee2742ffb3d838422802525a1db68d1a9
Xiejq83 <49154413+Xiejq83@users.noreply.github.com>
2019-11-08 11:26:13 +0800

    Add files via upload

--- SolvePt.m
+++ SolvePt.m
@@ -1,4 +1,4 @@
-% 瑙ｄ袱鏉＄洿绾跨殑浜ょ偣
+% 计算两条直线的交点
 function [x,y] = SolvePt(line1, line2)
     temp = line1(1)*line2(2) - line2(1)* line1(2);
     x = (line1(2)*line2(3) - line2(2)*line1(3))/ temp;
--- VanishLine.m
+++ VanishLine.m
@@ -31,10 +31,11 @@ function [coef] = VanishLine(lines)
     lines = temp;
     
     %% 去重
+    %若两条直线的相角差小于delta,则去掉其中的一条直线
     delta = pi/180;
     phi = atan((lines(:,1)./lines(:,2))*(-1));
     temp_phi = [phi(2: end); phi(1)];
-    delta_phi = abs(phi-temp_phi);
+    delta_phi = abs(phi-temp_phi); %相邻直线的相角差
     lines(delta_phi < delta, : ) = [];
     
     L = size(lines, 1);
@@ -43,6 +44,7 @@ function [coef] = VanishLine(lines)
     for m = 1: L-1
         [point(m,1),point(m,2)]= SolvePt(lines(m,:),lines(m+1,:));
     end
+    % point中存放相邻直线的交点
     [point(L,1),point(L,2)] = SolvePt(lines(L,:),lines(1,:));
 %     stem(point(:, 1), point(:, 2));
     minDis = 1e8;
@@ -51,12 +53,12 @@ function [coef] = VanishLine(lines)
     minP = [0,0];
     for m = 1:L-1
         for n = m+1:L
-            line1_index = [m+1: n-1];
-            line2_index = [1: m-1, n+1: L];
+            line1_index = [m+1: n-1]; %第一组点
+            line2_index = [1: m-1, n+1: L]; %第二组点
             dis1 = GroupVar(point(line1_index, : ))...
-                /sum(mean(point(line1_index, : )).^2);
+                /sum(mean(point(line1_index, : )).^2); %第一组点的归一化均方差
             dis2 = GroupVar(point(line2_index, : ))...
-                /sum(mean(point(line2_index, : )).^2);
+                /sum(mean(point(line2_index, : )).^2); %第二组点的归一化均方差
             dis = dis1+dis2;
             if dis < minDis
                 minDis = dis;
@@ -67,8 +69,8 @@ function [coef] = VanishLine(lines)
     end
     
     %% 拟合
-    line1_index = [minM+1: minN];
-    line2_index = [1:minM, minN+1: L];
+    line1_index = [minM+1: minN]; %第一组直线
+    line2_index = [1:minM, minN+1: L]; %第二组直线
 
 %     for m = 1: length(line1_index)
 %         tmp = lines(line1_index(m),:);
@@ -77,6 +79,7 @@ function [coef] = VanishLine(lines)
 %         refline(tmpk, tmpb);
 %     end
     
+    %分别取出两组直线的组内所有交点
     points1 = [];
     points2 = [];
     for m = 1: length(line1_index)-1
@@ -93,6 +96,8 @@ function [coef] = VanishLine(lines)
         end
     end
 %     stem(points2(:, 1), points2(:, 2));
+    
+    %两组点中分别舍去n/2个点
     point1 = Refine(points1, 1/2);
     point2 = Refine(points2, 1/2);
     points = [point1; point2];
